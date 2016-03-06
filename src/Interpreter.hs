75f41a4cd6618ac2e5cdd63edee866ee8ab80ec8
Raven Iqqe <raviqqe@gmail.com>
2016-03-06 22:58:52 +0900

    Read multiple lines on interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Interpreter (
   interpret
 ) where
@@ -24,11 +26,11 @@ interpret = runInterpreter interpretInputLines
   where
     interpretInputLines :: Interpreter ()
     interpretInputLines = do
-      inputLine <- lift $ getInputLine "ready> "
-      case inputLine of
+      inputLines <- lift $ getInputLines
+      case inputLines of
         Nothing   -> lift $ outputStrLn "Goodbye."
-        Just line -> do
-          processResult (parseToplevel sourceName line)
+        Just lines -> do
+          processResult (parseToplevel sourceName lines)
           interpretInputLines
       where
         processResult :: Either ParseError Toplevel -> Interpreter ()
@@ -41,6 +43,20 @@ interpret = runInterpreter interpretInputLines
           when (isExpr toplevel) $ do
             result <- liftIO $ eval newMod
             lift $ outputStrLn ("Evaluated to: " ++ result)
+
+getInputLines :: forall m . MonadException m => InputT m (Maybe String)
+getInputLines = promptInputLines (">>> ":repeat "... ") ""
+  where
+    promptInputLines :: forall m . MonadException m =>
+                        [String] -> String -> InputT m (Maybe String)
+    promptInputLines (prompt:prompts) oldLines = do
+      inputLine <- getInputLine prompt
+      case inputLine of
+        Nothing -> return Nothing
+        Just "" -> return (Just oldLines)
+        Just line -> promptInputLines prompts (oldLines ++ "\n" ++ line)
+    promptInputLines [] _ = fail "Prompts must be an infinite-length list of \
+                                 \strings."
 
 sourceName :: String
 sourceName = "<stdin>"
