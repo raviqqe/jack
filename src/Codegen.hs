5a4a8b309d174b1c009bc52fe94e847109d678ec
Raven Iqqe <raviqqe@gmail.com>
2016-03-10 00:26:49 +0900

    Codegen if expressions

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -75,6 +75,31 @@ codegenExpr (S.Var varName) = load =<< referToSymbol varName
 codegenExpr (S.Float num) = (return . constant . C.Float . F.Double) num
 codegenExpr (S.Call functionName args) = do
   call (globalRef double (Name functionName)) =<< mapM codegenExpr args
+codegenExpr (S.If cond exprIfTrue exprIfFalse) = do
+  thenBlock <- addBlock "if.then"
+  elseBlock <- addBlock "if.else"
+  exitIfBlock <- addBlock "if.exit"
+
+  cond <- fcmp FP.ONE false =<< codegenExpr cond
+  condbr cond thenBlock elseBlock
+
+  setBlock thenBlock
+  valueIfTrue <- codegenExpr exprIfTrue
+  br exitIfBlock
+
+  setBlock elseBlock
+  valueIfFalse <- codegenExpr exprIfFalse
+  br exitIfBlock
+
+  setBlock exitIfBlock
+  phi double [(valueIfTrue, thenBlock), (valueIfFalse, elseBlock)]
+codegenExpr (S.Boolean True) = return true
+codegenExpr (S.Boolean False) = return false
+
+true :: Operand
+true  = constant $ C.Float (F.Double 1.0)
+false :: Operand
+false = constant $ C.Float (F.Double 0.0)
 
 toSignatures :: [String] -> [(Type, Name)]
 toSignatures = map (\name -> (double, Name name))
